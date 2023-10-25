b1f2b48db958762b49b87c032f05f30d86751668
PavaniKolasani <94344360+PavaniKolasani@users.noreply.github.com>
2023-10-25 14:07:18 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -7,6 +7,7 @@ public static void main(String args[])
 system.out.println("Hello World");
 system.out.println("Hello everyone!");
 system.out.println("hi pavani");
+  system.out.println("hi Siri");
 
 }
 
